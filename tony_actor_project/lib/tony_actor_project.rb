3b75b46e8452c9aec82cffbb77a08015517c1e7b
Anthony Lepore <violacellonyc@gmail.com>
2021-06-19 18:27:20 +0000

    Automatically backed up by Learn

---
+++ tony_actor_project/lib/tony_actor_project.rb
@@ -0,0 +1,6 @@
+require "tony_actor_project/version"
+
+module TonyActorProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ tony_actor_project/lib/tony_actor_project/version.rb
@@ -0,0 +1,3 @@
+module TonyActorProject
+  VERSION = "0.1.0"
+end
